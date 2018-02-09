535df693b68602e5f25e8e1629083426ad0e880d
ivotedbury <35226323+ivotedbury@users.noreply.github.com>
2018-02-09 17:46:52 +0000

    180209 With GH Component

---
+++ BrickManager Component/BrickManager/BrickManager/BrickManagerInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace BrickManager
+{
+    public class BrickManagerInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "BrickManager";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("e7aa2165-27e3-479b-9777-fbdc93fbe707");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
